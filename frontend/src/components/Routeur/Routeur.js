e1089aeac3f4f83915402520bc43e75cff6d8307
Dagzounet <paul.gamot@gmail.com>
2023-11-10 22:36:25 +0100

    create and add the sign in webpage to react with necessary changes (className, htmlFor, ...) and add to routeur
    create and add the sign in webpage to react with necessary changes (className, htmlFor, ...) and add to routeur

--- frontend/src/components/Routeur/Routeur.js
+++ frontend/src/components/Routeur/Routeur.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../../pages/Home/Home";
+import SignIn from "../../pages/SignIn/SignIn";
 
 function Routeur() {
   return (
@@ -8,6 +9,7 @@ function Routeur() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </main>
     </Router>
