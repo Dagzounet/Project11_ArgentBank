d560d7f40d5fc38bd7d6d5856f0e02fa0f966c14
Dagzounet <paul.gamot@gmail.com>
2023-11-12 04:04:24 +0100

    create a fetch function to signIn to be able to connect user if email and password value are valid/registered in API server using state hooks, also using dispatch hooks to trigger my reducer to if it succed (store the token) or not, and also using selector hooks to change the state of isConnected or not, and navigate to user webpage when success
    create a fetch function to signIn to be able to connect user if email and password value are valid/registered in API server using state hooks, also using dispatch hooks to trigger my reducer to if it succed (store the token) or not, and also using selector hooks to change the state of isConnected or not, and navigate to user webpage when success

--- frontend/src/pages/SignIn/SignIn.js
+++ frontend/src/pages/SignIn/SignIn.js
@@ -1,32 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginSuccess, loginFailure } from "../../slices/loginSlice";
+import { useNavigate } from "react-router-dom";
 
 function SignIn() {
+  const dispatch = useDispatch();
+  const isConnected = useSelector((state) => state.loginSlice.isConnected);
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async (e) => {
+    e.preventDefault();
+
+    try {
+      console.log("Request Data:", JSON.stringify({ email, password }));
+      const response = await fetch("http://localhost:3001/api/v1/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      console.log("Server Response:", response);
+      const data = await response.json();
+
+      if (response.status === 200) {
+        dispatch(loginSuccess({ token: data.body.token }));
+        navigate("/user");
+      } else {
+        dispatch(loginFailure());
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      dispatch(loginFailure());
+    }
+  };
+
   return (
     <div className="Signin">
       <main className="main bg-dark">
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
-          <form>
+          <form onSubmit={signIn}>
             <div className="input-wrapper">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
+              <input
+                type="text"
+                id="username"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="input-wrapper">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
               <label htmlFor="remember-me">Remember me</label>
             </div>
-            {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-            <a href="./user" className="sign-in-button">
+            <button type="submit" className="sign-in-button">
               Sign In
-            </a>
-            {/* <!-- SHOULD BE THE BUTTON BELOW -->
-            <!-- <button className="sign-in-button">Sign In</button> -->
-            <!--  --> */}
+            </button>
+            {isConnected === false ? <p>Erreur d'identifiants</p> : null}
           </form>
         </section>
       </main>
